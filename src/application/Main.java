979e04f6f08ed07ef7259bca286ff071c23d6cc0
karlqvist97 <93927126+karlqvist97@users.noreply.github.com>
2021-12-26 09:27:00 +0100

    Added GUI elements, tab panes, and began working on the Controller
    Main:
    - Changed the root element from an AnchorPane to TabPane.
    - Adjusted the scene size to fit the size in the FXML file.
    View:
    - Added controls for finding and editing/deleting teachers.
    ViewController:
    - Declared variables for the GUI elements.
    - Began implementing an initialize method.

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,6 +3,7 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.TabPane;
 import javafx.scene.layout.AnchorPane;
 import javafx.fxml.FXMLLoader;
 
@@ -11,8 +12,8 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("View.fxml"));
-			Scene scene = new Scene(root,400,400);
+			TabPane root = (TabPane)FXMLLoader.load(getClass().getResource("View.fxml"));
+			Scene scene = new Scene(root,878,502);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
--- src/application/ViewController.java
+++ src/application/ViewController.java
@@ -1,5 +1,57 @@
 package application;
 
-public class ViewController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import model.Student;
+import model.Teacher;
+
+public class ViewController implements Initializable {
 	
+	@FXML private TabPane tabPane;
+	@FXML private Tab teacherTab;
+	@FXML private Tab studentTab;
+	
+	@FXML private Label lblErrMsgTName;
+	@FXML private Label lblErrMsgTSSN;
+	
+	@FXML private Label lblErrMsgTEmpNbr;
+	@FXML private Label lblErrMsgTNameNew;
+	@FXML private Label lblErrMsgTSSNNew;
+	
+	@FXML private Label lblTConf;
+	@FXML private Label lblTEditConf;
+	
+	@FXML private Button btnAddT;
+	@FXML private Button btnDelT;
+	@FXML private Button btnUpdateT;
+	
+	@FXML private TableView<Teacher> tvTeachers;
+	@FXML private TableColumn<Teacher, String> tcTEmpNbr;
+	@FXML private TableColumn<Teacher, String> tcTSSN;
+	@FXML private TableColumn<Teacher, String> tcTName;
+	@FXML private TableColumn<Teacher, Integer> tcTCourses;
+	
+	private ObservableList<Teacher> observableTeachers = FXCollections.observableArrayList();
+		
+	@Override
+	public void initialize(URL url, ResourceBundle resources) {
+		tcTEmpNbr.setCellValueFactory(new PropertyValueFactory<>("employeeNbr"));
+		tcTSSN.setCellValueFactory(new PropertyValueFactory<>("ssNbr"));
+		tcTName.setCellValueFactory(new PropertyValueFactory<>("name"));
+		tcTCourses.setCellValueFactory(new PropertyValueFactory<>("numberOfCourses")); //this won't work
+		
+		
+	} 
 }
